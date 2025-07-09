204519c9bfebb4ac75ca353362cadb94a27319f6
gem09lo <gemmaw09lo@gmail.com>
2025-07-09 13:18:21 +0100

    removed irrelevant files

--- coding_problems/02-tuesday/tuesday.py
+++
@@ -1,9 +0,0 @@
-def multi_table(number: int) -> str:
-    multiplication_table = []
-
-    for i in range(1, 11):
-        multiplication_table.append(f"{i} * {number} = {number * i}")
-    return "\n".join(multiplication_table)
-
-
-print(multi_table(5))
